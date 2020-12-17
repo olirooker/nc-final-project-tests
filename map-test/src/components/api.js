13619967a7efe11452129b8a08eed9a55c85088c
Alba Mateos <albmatcar@gmail.com>
2020-12-17 16:42:01 +0000

    make get request to the api

--- map-test/src/components/api.js
+++ map-test/src/components/api.js
@@ -8,12 +8,7 @@ const route = () => {
         'https://router.hereapi.com/v8/routes?transportMode=car&origin=52.5308,13.3847&destination=52.5264,13.3686&return=summary',
         {
           headers: {
-            Authorization: {
-              'Auth-Service-Id': 'here_app',
-              'Auth-Identifier': 'uKFp4S6JFSVOrzrGPXd1',
-              'Auth-Secret':
-                'HdksOE349TaVXjoOEX0r2WJzLZTZeEy9mjSSoEc2yyixanaGaOCUdu74bovAIzSbfGCqDn9ufJxg7zTGQxuO_A'
-            }
+            Authorization: `Bearer ${token}`
           }
         }
       )

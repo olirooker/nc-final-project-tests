bbadd0e7210f338e0be21a0503c4046d9f4c3b75
Oli Rooker <oliver.rooker@gmail.com>
2020-12-21 15:12:07 +0000

    firebase config changes in app

--- firebase-tests/firebase-test/src/App.js
+++ firebase-tests/firebase-test/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-// import Register from "./Components/Register";
-// import EmailSignIn from "./Components/EmailSignIn";
-import PhoneSignIn from "./Components/PhoneSignIn";
+import Register from "./Components/Register";
+import EmailSignIn from "./Components/EmailSignIn";
+// import PhoneSignIn from "./Components/PhoneSignIn";
 // import GoogleSignIn from "./Components/GoogleSignIn";
 
 function App() {
   return (
     <div className="app">
       {/* <GoogleSignIn /> */}
-      {/* <Register />
-      <EmailSignIn /> */}
-      <PhoneSignIn />
+      <Register />
+      <EmailSignIn />
+      {/* <PhoneSignIn /> */}
     </div>
   );
 }
